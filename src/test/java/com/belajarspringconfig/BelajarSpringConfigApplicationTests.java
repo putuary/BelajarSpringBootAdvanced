a3820bfc5e7e2f595529585a7905f1522cb0fd7b
putuary4223 <putuary4223@gmail.com>
2024-08-24 19:52:10 +0700

    first commit

---
+++ src/test/java/com/belajarspringconfig/BelajarSpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajarspringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/belajarspringconfig/testing/ResourceTest.java
@@ -0,0 +1,17 @@
+package com.belajarspringconfig.testing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class ResourceTest {
+    @Test
+    void testResource() throws IOException {
+        var resource = new ClassPathResource("/text/sample.txt");
+        Assertions.assertNotNull(resource);
+        Assertions.assertTrue(resource.exists());
+        Assertions.assertNotNull(resource.getFile());
+    }
+}
